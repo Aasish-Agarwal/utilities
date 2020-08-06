65f21959d80cb9722382b49efe34982476fd19a1
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2020-08-06 18:46:18 +0530

    Delete consolidate.bat

--- python/collate-effort-data/consolidate.bat
+++
@@ -1,4 +0,0 @@
-echo WorkStream,Iteration,Member,Day,Activity,Effort > consolidated.csv
-python collateMemberData.py WorkStream1.xlsm >> consolidated.csv
-python collateMemberData.py WorkStream2.xlsm >> consolidated.csv
-python collateMemberData.py WorkStream3.xlsm >> consolidated.csv
